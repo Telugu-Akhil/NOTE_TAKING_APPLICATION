17e33d45ab1cd230c07d5152bb77fbdf6d584912
Telugu-Akhil <102343028+Telugu-Akhil@users.noreply.github.com>
2023-03-18 14:17:24 +0530

    Initial commit

---
+++ src/components/Notes.jsx
@@ -0,0 +1,14 @@
+const react = require("react");
+
+function Notes() {
+  return (
+    <div className="note">
+      <h1> React.js Bootcamp</h1>
+      <p>
+        We learnt the basics of Web~Development,JavaScript,JSX, React and
+        Created a project at very end of bootcamp as well.
+      </p>
+    </div>
+  );
+}
+export default Notes;
